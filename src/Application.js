791c6e067538d7cb70a50e8df86464626b214b1f
Vladimir <tiunov2008.va@gmail.com>
2022-10-30 19:00:15 +0300

    test

--- src/Application.js
+++ src/Application.js
@@ -9,7 +9,7 @@ export function Application() {
     <div>
       <nav>
         <Link data-testid="link-home" to="/">
-          Home
+          Home1
         </Link>
         <Link to="/about">About</Link>
       </nav>
